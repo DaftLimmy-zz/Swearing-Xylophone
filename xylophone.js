e74963da03913d42ca62dbbf070ac6b3b2dd06be
Lev <levdevuk@gmail.com>
2020-05-23 07:59:59 +0100

    Removed manifest array

--- xylophone.js
+++ xylophone.js
@@ -1,56 +1,13 @@
-let wordNumber = 0;
+let wordNumber = 1;
 const soundsPath = 'sounds/';
 const extension = '.mp3';
 
-const manifest = [
-  [
-    // You
-    '1_1',
-    '1_2',
-    '1_3',
-    '1_4',
-    '1_5',
-  ],
-  [
-    // Are
-    '2_1',
-    '2_2',
-    '2_3',
-    '2_4',
-    '2_5',
-  ],
-  [
-    // A
-    '3_1',
-    '3_2',
-    '3_3',
-    '3_4',
-    '3_5',
-  ],
-  [
-    // Fucking
-    '4_1',
-    '4_2',
-    '4_3',
-    '4_4',
-    '4_5',
-  ],
-  [
-    // Cunt
-    '5_1',
-    '5_2',
-    '5_3',
-    '5_4',
-    '5_5',
-  ],
-];
-
 function playSound(target) {
-  new Audio(soundsPath + manifest[wordNumber][target.id] + extension).play();
+  new Audio(`${soundsPath + wordNumber}_${target.id + extension}`).play();
 
-  if (wordNumber < 4) {
+  if (wordNumber < 5) {
     wordNumber += 1;
   } else {
-    wordNumber = 0;
+    wordNumber = 1;
   }
 }
